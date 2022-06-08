150208723e536fbbefdbfebdb53d2c43056978e6
Zach French <102378716+Zfrench741@users.noreply.github.com>
2022-06-08 11:54:12 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+// *Variables*
+// Declare a variable, assign it a boolean, and alert the value
+
+
+// Declare a variable, reassign it to your favorite color, and console log the value
+
+
+// *Functions*
+// Create a function that takes in 4 numbers and returns the sum of the first 3 numbers divided by the fourth. Return the result. Call the function.
+function subFourNums (n1,n2,n3,n4) {
+    (n1+n2+n3) / n4
+}
+subFourNums (10,5,15,2)
+// Create a function that takes in 2 numbers. Console log the first number to the power of the second. Call the function.
+
+function twoNums (n1,n2) {
+    console.log(n1)
+}
+
+
+// *Conditionals*
+// Create a function that takes in a boolean and a string. If the boolean is true, alert the string. If the boolean is false, console log the string
+function takeTwoNums (n1,n2) {
+    let age = boolen & '3'
+    if (age = boolen && '3')
+    console.log(true)
+}else if (boolen = time)
+console.log('3')
+
+//*Loops*
+//Create a function that takes in a number. Console log all values from 1 to that number, but if the number is divisible by 3 log "fizz" instead of that number, if the number is divisible by 5 log "buzz" instead of the number, and if the number is divisible by 3 and 5 log "fizzbuzz" instead of that number
